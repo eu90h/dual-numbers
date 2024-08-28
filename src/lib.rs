4df8c2e75fe9d41a0230ec7d83751b54f9eae7e0
Stefan <stefan@eu90h.com>
2024-08-28 17:23:45 -0400

    Renamed exp to powi. Added some operations and a new test.

--- src/lib.rs
+++ src/lib.rs
@@ -10,12 +10,28 @@ impl DualNumber {
         DualNumber { a, b }
     }
 
-    pub fn exp(self, n: i64) -> Self {
+    pub fn powi(self, n: i64) -> Self {
         let mut x = self;
-        for i in 1..n {
+        for _ in 1..n {
             x *= self;
         }
         x
+    }
+
+    pub fn log(self, base: f64) -> Self {
+        DualNumber::new(self.a.log(base), 1.0/self.a * self.b)
+    }
+
+    pub fn exp(self) -> Self {
+        DualNumber::new(self.a.exp(), self.a.exp() * self.b)
+    }
+
+    pub fn sin(self) -> Self {
+        DualNumber::new(self.a.sin(), self.a.cos() * self.b)
+    }
+
+    pub fn cos(self) -> Self {
+        DualNumber::new(self.a.cos(), self.a.sin() * self.b)
     }
 }
 
@@ -24,6 +40,25 @@ impl std::ops::MulAssign for DualNumber {
         *self = *self * rhs;
     }
 }
+
+impl std::ops::AddAssign for DualNumber {
+    fn add_assign(&mut self, rhs: Self) {
+        *self = *self + rhs;
+    }
+}
+
+impl std::ops::SubAssign for DualNumber {
+    fn sub_assign(&mut self, rhs: Self) {
+        *self = *self - rhs;
+    }
+}
+
+impl std::ops::DivAssign for DualNumber {
+    fn div_assign(&mut self, rhs: Self) {
+        *self = *self / rhs;
+    }
+}
+
 
 impl std::ops::Add for DualNumber {
     type Output = DualNumber;
@@ -80,7 +115,16 @@ mod tests {
         let b = DualNumber::new(3., 0.);
         let c = DualNumber::new(4., 1.);
         //dc = 1 + 6c^5 = 6145 
-        let result = a*b + c + c.exp(6); //4106
+        let result = a*b + c + c.powi(6); //4106
         assert_eq!(result, DualNumber::new(4106., 6145.));
+    }
+
+    #[test]
+    fn log_and_sin() {
+        let a = DualNumber::new(2., 1.);
+        let b = DualNumber::new(5., 0.);
+        let y = a.log(1.0_f64.exp()) + a * b - b.sin();
+        assert!((y.a - 11.652) < 0.0001);
+        assert!(y.b - 5.5 < 0.0001)
     }
 }
